98f371e70d8debffebb60312ac49f10be92b19f7
karansidepanda <123609539+karansidepanda@users.noreply.github.com>
2023-05-03 12:26:06 +0530

    My Changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
-  return (
+function FetchData(){
+  const [post , setpost] = useState({})
+  const [id, setid] = useState ([])
+  const [idfrombutton, setIdFromButton] = useState ([])
+  const handleClick = () => {
+    setIdFromButton(id)
+  }
+
+  useEffect (() => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts/${idfrombutton}`)
+    .then(res =>{
+      setpost(res.data)
+    }
+      )
+  }, [idfrombutton]
+  )
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+      <div className="input-group">
+          <label htmlFor="id">Order Id</label>
+          <input type="text" value={id} onChange={e => setid(e.target.value)} />
+        </div>
+        <div className="input-group">
+          <label htmlFor="url">Shop URL</label>
+          <input type="text" id="uid" />
+        </div>
+        <button type="button" onClick={handleClick}>Fetch Order Detail</button>
+      </form>
+      {idfrombutton.length > 0 && (
+            <pre>{JSON.stringify(post, null, 2)}</pre>
+      )
+      
+}
+      
     </div>
-  );
+  )
+
+
+
+
 }
 
-export default App;
+export default FetchData;
