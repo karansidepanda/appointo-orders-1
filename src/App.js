711a2c365b4e6d2e2142effd48afdc93cdb58b89
karansidepanda <123609539+karansidepanda@users.noreply.github.com>
2023-05-26 12:42:38 +0530

    Update App.js
    PUt Request chage

--- src/App.js
+++ src/App.js
@@ -5,29 +5,35 @@ import axios from "axios";
 function FetchData(){
   const [post , setpost] = useState({})
   const [id, setid] = useState ([])
+  const [url, seturl] = useState ([])
   const [idfrombutton, setIdFromButton] = useState ([])
+  const [urlfrombutton, seturlFromButton] = useState ([])
   const handleClick = () => {
     setIdFromButton(id)
+    seturlFromButton(url)
   }
 
   useEffect (() => {
-    axios.get(`https://jsonplaceholder.typicode.com/posts/${idfrombutton}`)
+    axios.put(`https://app.appointo.me/scripttag/shopify_endpoint`, {
+      shopify_domain: url,
+      endpoint: id
+  })
     .then(res =>{
       setpost(res.data)
     }
       )
-  }, [idfrombutton]
+  }, [idfrombutton, urlfrombutton]
   )
   return(
     <div className="App">
       <form>
       <div className="input-group">
-          <label htmlFor="id">Order Id</label>
+          <label htmlFor="id">Endpoint</label>
           <input type="text" value={id} onChange={e => setid(e.target.value)} />
         </div>
         <div className="input-group">
           <label htmlFor="url">Shop URL</label>
-          <input type="text" id="uid" />
+          <input type="text" value={url} onChange={e => seturl(e.target.value)} />
         </div>
         <button type="button" onClick={handleClick}>Fetch Order Detail</button>
       </form>
